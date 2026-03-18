e107a5655222af8f121c7cd702ecbbfc87d980a5
amaxbuteneers666-art <amaxbuteneers666@gmail.com>
2026-03-19 04:56:52 +0700

    add script carController

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    public WheelColliders colliders;
+    public WheelMeshes wheelMeshes;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,10 +16,19 @@ public class CarController : MonoBehaviour
         
     }
 }
+[System.Serializable]
 public class WheelColliders
 {
     public WheelCollider FRWheel;
     public WheelCollider FLWheel;
     public WheelCollider RRWheel;
     public WheelCollider RLWheel;
+}
+[System.Serializable]
+public class WheelMeshes
+{
+    public MeshRenderer FRWheel;
+    public MeshRenderer FLWheel;
+    public MeshRenderer RRWheel;
+    public MeshRenderer RLWheel;
 }
