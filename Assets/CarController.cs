5a827f720d3256b4a743b362e5ab459431132f5c
amaxbuteneers666-art <amaxbuteneers666@gmail.com>
2026-03-19 04:50:42 +0700

    add collider to car

---
+++ Assets/CarController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public class WheelColliders
+{
+    public WheelCollider FRWheel;
+    public WheelCollider FLWheel;
+    public WheelCollider RRWheel;
+    public WheelCollider RLWheel;
+}
